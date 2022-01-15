49d246659aa5f746d2e1689578dd265f63c2e68f
Ron Superior <97489744+rsuperior@users.noreply.github.com>
2022-01-14 21:36:23 -0500

    Changed the JSON format to one I can more easily deserialize in C#
    On the ClassicUO side of things, obviously.

--- guardlines-def-to-json.py
+++ guardlines-def-to-json.py
@@ -7,50 +7,52 @@ from json import dump
 from rect import Rect, group_adjacent_rects, \
         adjacent_rects_corners_to_polygons, find_intersections_in_rects
 
-def rects_to_polygons(zonelevel, warn_intersections):
-    for k in zonelevel.keys():
-        rects = zonelevel[k]
+def rects_to_polygons(rects, warn_intersections):
+    if warn_intersections:
+        intersections = find_intersections_in_rects(rects)
 
-        if warn_intersections:
-            intersections = find_intersections_in_rects(rects)
+        # I'm not sure intersections are a problem per se, but I think
+        # it's a good idea to visually inspect the polygons that
+        # come out of this program around any area where there were
+        # intersecting rectangles in the input.
 
-            # I'm not sure intersections are a problem per se, but I think
-            # it's a good idea to visually inspect the polygons that
-            # come out of this program around any area where there were
-            # intersecting rectangles in the input.
+        if intersections:
+            print("Intersections found!", intersections, file=sys.stderr)
 
-            if intersections:
-                print("Intersections found in", k, "!", intersections,
-                        file=sys.stderr)
+    polygons = []
 
-        polygons = []
+    for g in group_adjacent_rects(rects):
+        if len(g) > 1:
+            polygons.extend(
+                adjacent_rects_corners_to_polygons([r.corners() for r in g])
+            )
+        else:
+            polygons.append(g[0].polygon())
 
-        for g in group_adjacent_rects(rects):
-            if len(g) > 1:
-                polygons.extend(
-                    adjacent_rects_corners_to_polygons([r.corners() for r in g])
-                )
-            else:
-                polygons.append(g[0].polygon())
+    return polygons
 
-        zonelevel[k] = polygons
 
+def add_zones_from_rects(zones, rects, color, label, warn_intersections):
+    zones.extend([
+        {'polygon': p, 'color': color, 'label':label} \
+            for p in rects_to_polygons(rects, warn_intersections)
+    ])
 
 def convert_def_to_json(inf, outf, mapindex, color, warn_intersections):
     rgx_should_parse_ints = compile(r'^\d')
     rgx_parse_ints = compile(r'(-?\d+)')
     rgx_parse_comment = compile(r'^#(.+)$')
 
-    maplevel = {'color': color, 'zones': {} }
-    output = { mapindex: maplevel }
-    zonename = 'unnamed'
+    output = { 'mapIndex': mapindex, 'zones': [] }
+    label = 'unnamed'
     rects = []
 
     for line in sys.stdin:
         if m := rgx_parse_comment.match(line):
             if len(rects):
-                maplevel['zones'][zonename] = rects
-            zonename = m.group(1).strip()
+                add_zones_from_rects(output['zones'], rects, color, label,
+                    warn_intersections)
+            label = m.group(1).strip()
             rects = []
         elif rgx_should_parse_ints.match(line):
             six_ints = tuple(map(int, rgx_parse_ints.findall(line)))
@@ -60,9 +62,8 @@ def convert_def_to_json(inf, outf, mapindex, color, warn_intersections):
             rects.append(Rect(*six_ints[:4]))
 
     if len(rects):
-        maplevel['zones'][zonename] = rects
-
-    rects_to_polygons(maplevel['zones'], warn_intersections)
+        add_zones_from_rects(output['zones'], rects, color, label,
+                warn_intersections)
 
     dump(output, outf, sort_keys=True, indent=4)
 
@@ -82,8 +83,8 @@ new (Jan 2022) 'Zones' feature for the 'World Map'."""
 
     parser.add_option("-m", "--mapindex", type="int", default=0,
         help="Set 'World.MapIndex' to which these zones belong (default 0)")
-    parser.add_option("-c", "--color", type="string", default="green",
-        help="Set color these zones should be drawn (default green)")
+    parser.add_option("-c", "--color", type="string", default="yellow",
+        help="Set color these zones should be drawn (default yellow)")
     parser.add_option("-w", "--warn-intersections", action='store_true',
         help="Warn on stderr about intersecting rects within groups",
         default=False)
